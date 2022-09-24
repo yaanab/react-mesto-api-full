3e3253a351ef51a27ba4b92d9e3101e95c828b77
yaana <ya.anastasya.b@yandex.ru>
2022-09-24 18:28:57 +0300

    feat: замена url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'api.yaana.nomoredomains.s.nomorepartiesxyz.ru';
 
 const testResponse = (res) => {
   if (res.ok) {
