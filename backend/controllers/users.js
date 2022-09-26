7449405ea5472ac60d453bc0376aea00e1b2082a
yaana <ya.anastasya.b@yandex.ru>
2022-09-26 20:42:56 +0300

    fix: sameSite false

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -116,8 +116,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: 'none',
-          secure: true,
+          sameSite: false,
         });
       res.send({ token });
     })
