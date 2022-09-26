1b6f72e6b7d7902140dc5f2f7e2a5a90833a74b0
yaana <ya.anastasya.b@yandex.ru>
2022-09-26 18:53:35 +0300

    fix: без secure

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -116,8 +116,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: 'none',
-          secure: true,
+          sameSite: false,
+          // secure: true,
         });
       res.send({ token });
     })
