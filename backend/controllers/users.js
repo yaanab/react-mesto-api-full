df4de5aa956c1c1af94d8ba9d8e202df091565a4
yaana <ya.anastasya.b@yandex.ru>
2022-09-26 19:00:02 +0300

    feat: замена base_url

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -117,7 +117,6 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: false,
-          // secure: true,
         });
       res.send({ token });
     })
